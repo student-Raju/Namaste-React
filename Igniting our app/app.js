05700d50de392e17321ec6fb06630b7e6fa4f490
Rajgopal Mahata <157442234+student-Raju@users.noreply.github.com>
2024-05-16 22:03:34 +0530

    episode-02

--- Igniting our app/app.js
+++ Igniting our app/app.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
 const parent=React.createElement("div",{id:"parent"},[
     React.createElement("div",{id:"child"},[
     React.createElement("div",{},"I am an h1 tag"),
